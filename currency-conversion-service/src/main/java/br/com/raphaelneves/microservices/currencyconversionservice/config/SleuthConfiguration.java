49006e58636e422303df4249439169a883eac470
raphaeloneves <raphael.neves@penguinformula.com>
2018-07-20 16:32:06 +0100

    Preparing services to get traced with request unique id

---
+++ currency-conversion-service/src/main/java/br/com/raphaelneves/microservices/currencyconversionservice/config/SleuthConfiguration.java
@@ -0,0 +1,13 @@
+package br.com.raphaelneves.microservices.currencyconversionservice.config;
+
+import brave.sampler.Sampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SleuthConfiguration {
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
---
+++ gateway-server/src/main/java/br/com/raphaelneves/microservices/gatewayserver/config/SleuthConfiguration.java
@@ -0,0 +1,14 @@
+package br.com.raphaelneves.microservices.gatewayserver.config;
+
+import brave.sampler.Sampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SleuthConfiguration {
+
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
