9160a28f31cb0c865010e5fcddf611a0ebfc56e9
raphaeloneves <raphael.neves@penguinformula.com>
2018-07-20 15:40:35 +0100

    Adding Zuul and request logging filtering

---
+++ gateway-server/src/main/java/br/com/raphaelneves/microservices/gatewayserver/LoggerFilter.java
@@ -0,0 +1,38 @@
+package br.com.raphaelneves.microservices.gatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoggerFilter extends ZuulFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        logger.info("request URI -> {} | request -> {}", request.getRequestURI(), request);
+        return null;
+    }
+}
