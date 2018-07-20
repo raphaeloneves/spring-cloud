2cec5024255b0baf3e4d9d0552047d35f26d4ce9
raphaeloneves <raphael.neves@penguinformula.com>
2018-07-20 16:09:26 +0100

    Integrating currency conversion service to use gateway server

--- currency-conversion-service/src/main/java/br/com/raphaelneves/microservices/currencyconversionservice/integrations/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/br/com/raphaelneves/microservices/currencyconversionservice/integrations/CurrencyExchangeServiceProxy.java
@@ -6,10 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("currency-exchange-service")
+//@FeignClient("currency-exchange-service")
+@FeignClient("gateway-server")
 @RibbonClient("currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-    String BASE_SERVICE_URL = "currency-exchange";
+    String BASE_SERVICE_URL = "/currency-exchange-service/currency-exchange";
 
     @GetMapping(value = BASE_SERVICE_URL + "/from/{from}/to/{to}")
     CurrencyConversion getCurrencyExchangeFromTo(@PathVariable("from") String from, @PathVariable("to") String to);
