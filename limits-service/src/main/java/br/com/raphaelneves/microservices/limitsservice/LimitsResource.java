d6456d2f6b7267c8f2474049dd512c482b9389ac
raphaeloneves <raphael.neves@penguinformula.com>
2018-07-23 10:42:32 +0100

    Added graceful beahavior for limits service (handle fault tolerance)

--- limits-service/src/main/java/br/com/raphaelneves/microservices/limitsservice/LimitsResource.java
+++ limits-service/src/main/java/br/com/raphaelneves/microservices/limitsservice/LimitsResource.java
@@ -2,6 +2,7 @@ package br.com.raphaelneves.microservices.limitsservice;
 
 import br.com.raphaelneves.microservices.limitsservice.bean.Limit;
 import br.com.raphaelneves.microservices.limitsservice.config.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,16 @@ public class LimitsResource {
     @GetMapping(value = "")
     public Limit getLimitsConfiguration(){
         return new Limit(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+    @GetMapping(value = "/fault")
+    @HystrixCommand(fallbackMethod = "faultRetrieve")
+    public Limit getLimitsConfigurationWithFault(){
+        throw new RuntimeException("Service cannot be reached.");
+    }
+
+    private final Limit faultRetrieve(){
+        return new Limit(9, 999);
     }
 
 }
